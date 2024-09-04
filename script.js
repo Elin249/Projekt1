2f6cc65cc8dc7f06bba8bd97a157c99b36cad62a
Elin249 <elin.m.zarouki@gmail.com>
2024-09-04 19:15:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript för framtida interaktivitet
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Kafé Harmoni sida laddad.");
+    
+    // Lägg till mer interaktivitet här om det behövs
+});
